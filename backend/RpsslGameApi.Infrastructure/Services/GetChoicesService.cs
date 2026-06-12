76cdc25829a90588e983f9814745e38f4fddab03
carulena <anes.2017@alunos.utfpr.edu.br>
2026-06-12 16:35:52 -0300

    feat: add service - GetChoices

---
+++ backend/RpsslGameApi.Infrastructure/Services/GetChoicesService.cs
@@ -0,0 +1,19 @@
+﻿using RpsslGameApi.Contracts;
+using RpsslGameApi.Infrastructure.Mappers.Interfaces;
+using RpsslGameApi.Infrastructure.Services.Interfaces;
+namespace RpsslGameApi.Infrastructure.Services;
+
+public class GetChoicesService:IGetChoicesService
+{
+    private readonly IGetChoicesMapper _choicesMapper;
+
+    public GetChoicesService(IGetChoicesMapper choicesMapper)
+    {
+        _choicesMapper = choicesMapper;
+    }
+    
+    public IEnumerable<ChoiceResponse> FetchChoices()
+    {
+        return _choicesMapper.GetChoices();
+    }
+}
