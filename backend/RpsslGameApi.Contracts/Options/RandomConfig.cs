fbb2bb58dfe7420b9c24618e44ee998f25d8a757
carulena <anes.2017@alunos.utfpr.edu.br>
2026-06-12 16:34:49 -0300

    feat: add contracts

---
+++ backend/RpsslGameApi.Contracts/Options/RandomConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace RpsslGameApi.Contracts.Options;
+
+public class RandomConfig
+{
+    public string url { get; set; }
+}
---
+++ backend/RpsslGameApi.Contracts/RandomNumberResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RpsslGameApi.Contracts;
+
+public class RandomNumberResponse
+{
+    [JsonPropertyName("random_number")]
+    public required int RandomNumber { get; set; }
+}
