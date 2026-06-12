f4db0b4ee5d6c2c4009a8c5245338300e019bd18
carulena <anes.2017@alunos.utfpr.edu.br>
2026-06-12 16:35:20 -0300

    feat: configure dependencies

---
+++ backend/RpsslGameApi.Application/StartupExtensions/ConfigureDependenciesExtensions.cs
@@ -0,0 +1,40 @@
+﻿using RpsslGameApi.Contracts.Options;
+using RpsslGameApi.Infrastructure.Mappers;
+using RpsslGameApi.Infrastructure.Mappers.Interfaces;
+using RpsslGameApi.Infrastructure.Repositories;
+using RpsslGameApi.Infrastructure.Repositories.Interface;
+using RpsslGameApi.Infrastructure.Services;
+using RpsslGameApi.Infrastructure.Services.Interfaces;
+
+namespace RpsslGameApi.Application.StartupExtensions;
+
+public static class  ConfigureDependenciesExtensions
+{
+    public static void ConfigureDependencies(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        services.AddHttpClient<IGetRandomNumberRepository, GetRandomNumberRepository>();
+        services.Configure<RandomConfig>(configuration.GetSection("RandomConfig"));
+
+        #region Services
+
+        services.AddScoped<IPlayService, PlayService>();
+        services.AddScoped<IGetChoiceService, GetChoiceService>();
+        services.AddScoped<IGetChoicesService, GetChoicesService>();
+
+        #endregion
+        
+        #region Mapper
+
+        services.AddSingleton<IPlayMapper, PlayMapper>();
+        services.AddSingleton<IGetChoiceMapper, GetChoiceMapper>();
+        services.AddSingleton<IGetChoicesMapper, GetChoicesMapper>();
+
+        #endregion
+        
+        #region Repositories
+
+        services.AddSingleton<IGetRandomNumberRepository, GetRandomNumberRepository>();
+
+        #endregion
+    }
+}   
